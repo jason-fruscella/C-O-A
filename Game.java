0f4e1dd6479e4152443b7063484d9c75eac95bce
vsenapat <senvi04@ca.com>
2014-12-03 14:45:23 -0600

    Vijay_Jason

---
+++ Game.java
@@ -0,0 +1,15 @@
+package Initials;
+
+public class Game {
+	public void initialize(){
+		initials("VSJF");
+	}
+
+	/**
+	 * initialize with initials
+	 * @param string
+	 */
+	private void initials(String string) {
+				
+	}
+}
